afc4771f700cb01bf8a338ac27b788a3620dd19d
David <2181042@my.ipleiria.pt>
2021-01-17 20:25:12 +0000

    sensorReadind

---
+++ Lumisusidade_LTR329ALS01/sloeber.ino.cpp
@@ -0,0 +1,16 @@
+#ifdef __IN_ECLIPSE__
+//This is a automatic generated file
+//Please do not modify this file
+//If you touch this file your change will be overwritten during the next build
+//This file has been generated on 2021-01-17 20:18:57
+
+#include "Arduino.h"
+#include <Wire.h>
+
+void setup() ;
+void loop() ;
+
+#include "Lumisusidade_LTR329ALS01.ino"
+
+
+#endif
