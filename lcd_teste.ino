662d6ea3f9fa46d8567216d4cf1b49b2263aa35e
2181665 <57800769+2181665@users.noreply.github.com>
2021-02-01 15:09:34 +0000

    BLE, LCD e I2C check

---
+++ lcd_teste.ino
@@ -0,0 +1,14 @@
+#include "Arduino.h"
+#include "TFT_eSPI.h" // Hardware-specific library
+#include <SPI.h>
+//The setup function is called once at startup of the sketch
+void setup()
+{
+// Add your initialization code here
+}
+
+// The loop function is called in an endless loop
+void loop()
+{
+//Add your repeated code here
+}
